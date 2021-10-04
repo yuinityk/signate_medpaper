d306457e6df9c7f1d8a70af5f11bc843898bd5a0
yuinityk <yuuki.initial.yk@gmail.com>
2021-10-04 23:15:46 +0900

    sa

--- src/validation/eval_model.py
+++ src/validation/eval_model.py
@@ -70,6 +70,54 @@ def test_title_abst_concat(df_test, args, fold):
 
     return prob_test
 
+def test_title_abst_concat_nofold(df_test, args):
+    test_dataset = SRTitleAbstConcatenateDataset(
+        df_test,
+        args.model_name,
+        max_length=args.max_length,
+        train=False
+    )
+
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        drop_last=False,
+        num_workers=args.num_workers
+    )
+
+    dropout = None
+    if args.dropout is not None:
+        dropout = args.dropout
+
+    config = transformers.AutoConfig.from_pretrained(args.model_name)
+    config.num_labels = 1
+    if dropout is not None:
+        config.hidden_dropout_prob = dropout
+        config.attention_probs_dropout_prob = dropout
+
+    model = SRTitleClassifyTransformer(args.model_name, config=config)
+    model.load_state_dict(torch.load(os.path.join(args.save_path, f"{args.trial_name}-epoch_1.bin")))
+    model.to(args.device)
+
+    out_dict = {}
+    with torch.no_grad():
+        t = tqdm(test_loader)
+        for i, (id_, input_ids_title, attention_mask_title) in enumerate(t):
+            input_ids_title = input_ids_title.to(args.device)
+            attention_mask_title = attention_mask_title.to(args.device)
+
+            output = model(
+                input_ids_title=input_ids_title,
+                attention_mask_title=attention_mask_title
+            )
+            for id_tmp, out_tmp in zip(id_.numpy(), output.detach().cpu().numpy()):
+                out_dict[id_tmp] = out_tmp
+
+    prob_test = pd.DataFrame(out_dict.values(), index=out_dict.keys()).rename({0: f"prob"})
+
+    return prob_test
+
 def ensemble_folds(prob_test, args, ensemble_type):
     prob_array = prob_test.values
 
@@ -111,6 +159,27 @@ def main_title_abst_concat(args):
     for ensemble_type in ["prob_mean", "pred_vote"]:
         df_pred = ensemble_folds(prob_test, args, ensemble_type).sort_index()
         df_pred.to_csv(os.path.join(args.save_path, f"submit_{args.trial_name}_{ensemble_type}.csv"), index=True, header=False)
+
+
+def main_title_abst_concat_nofold(args):
+    seed_torch(args.seed)
+
+    os.makedirs(args.save_path, exist_ok=True)
+
+    dataset = ClassifyDataset(dir_path=args.dir_path)
+    df_train = dataset.get(args.fname_train, args.id_to_1, args.id_to_0)
+    df_test = dataset.get(args.fname_test)
+    if args.thr is None:
+        thr = df_train['judgement'].mean()
+        args.thr = thr
+
+    prob_test = test_title_abst_concat_nofold(df_test, args)
+    prob_test.to_csv(os.path.join(args.save_path, f"prob_test_{args.trial_name}.csv"), index=True, header=True)
+
+    for ensemble_type in ["prob_mean", "pred_vote"]:
+        df_pred = ensemble_folds(prob_test, args, ensemble_type).sort_index()
+        df_pred.to_csv(os.path.join(args.save_path, f"submit_{args.trial_name}_{ensemble_type}.csv"), index=True, header=False)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
